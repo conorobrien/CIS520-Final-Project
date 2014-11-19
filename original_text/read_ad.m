938d4eb4ade4381efe2d98fb5d465ff3e056d1b8
Nick Hu <siyaohu@seas.upenn.edu>
2014-11-19 13:02:40 -0500

    changed read_ad to function, argin is the ad index, output in workspace the ad

--- original_text/read_ad.m
+++ original_text/read_ad.m
@@ -1,9 +1,12 @@
-%read in the file you want, here we use train_ad.txt
+function read_ad(N)
+%read in the file you want, here we use train_ad.tx
 fid=fopen('train_ad.txt');
 %N is the index of the ad you want
 %Example here display the 2nd ad for train_ad 
 %total number of training ads=20311
 %total number of test ads=20307
-N=2;
+N
 line=textscan(fid,'%s', 1, 'delimiter', '\n','HeaderLines', N-1);
 line{1}{1}
+
+end
