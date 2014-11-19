5fc53d5e5f5c21f6f59feda6532748a9b64b4fc4
Nick Hu <siyaohu@seas.upenn.edu>
2014-11-19 15:08:25 -0500

    added Train and Test Word Count

--- original_text/getTestWordCount.m
+++ original_text/getTestWordCount.m
@@ -1,14 +1,15 @@
-function count = getWordCount()
+function count = getTestWordCount()
 
-fid=fopen('train_ad.txt');
+
+fid=fopen('test_ad.txt');
 %N is the index of the ad you want
 %Example here display the 2nd ad for train_ad 
 %total number of training ads=20311
 %total number of test ads=20307
-count = zeros(20311,1);
+count = zeros(20307,1);
 lines = textscan(fid, '%s', 'delimiter', '\n');
 
-for i = 1:20311
+for i = 1:20307
     line=lines{1}{i};
     d = textscan(line, '%s', 'delimiter', ' ');
     count(i) = size(d{1},1);
