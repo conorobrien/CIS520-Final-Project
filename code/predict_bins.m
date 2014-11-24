db2241881042c90c93ed4e707b01905a24551b36
Conor O'Brien <conoreobrien@gmail.com>
2014-11-24 18:18:59 -0500

    Predict_bins now works

--- code/predict_bins.m
+++ code/predict_bins.m
@@ -1,14 +1,15 @@
-function [Y_pred_bins, Y_bins] = predict_bins(X, Y, X_test, n_bins);
+function [Y_pred_bins, Y_bins] = predict_bins(X, Y, X_test, n_bins)
+    addpath('liblinear/matlab')
 
-    bin_price = min(Y_train):((max(Y_train) - min(Y_train))/n_bins):max(Y_train);
+    bin_price = min(Y):((max(Y) - min(Y))/n_bins):max(Y);
     Y_bins = zeros(size(Y));
     
     for i = 2:(n_bins+1)
-        Y_bins(Y_train>=bin_price(i-1) & Y_train<=bin_price(i)) = i-1;
+        Y_bins(Y>=bin_price(i-1) & Y<=bin_price(i)) = i-1;
     end
 
-    svm_model = train(Y_bins,X,[,'-t 3']); %#ok<NBRAK,NOCOM>
+    svm_model = train(Y_bins,X,[,'-s 5']); %#ok<NBRAK,NOCOM>
 
-    Y_pred_bins = predict(rand(length(X_test),1),X_pca_test, svm_model);
+    Y_pred_bins = predict(rand(length(X_test),1),X_test, svm_model);
 
 end
