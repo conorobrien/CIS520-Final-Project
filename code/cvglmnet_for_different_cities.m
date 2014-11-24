a1f357c96f5f26f69d30c5d4ba43a19457a29f94
Nick Hu <siyaohu@seas.upenn.edu>
2014-11-24 16:16:40 -0500

    updated cvglmnet_for_different_cities.m

--- code/cvglmnet_for_different_cities.m
+++ code/cvglmnet_for_different_cities.m
@@ -1,21 +1,5 @@
-%% Subtract mean of city before doing fit, then add it back after predicting.
 
-% meancity = zeros(7,1);
-% ind = zeros(numel(Y_train),1);
-% testind = zeros(size(X_test,1),1);
-% for i = 1:7
-%     meancity(i) = mean(Y_train(X_train(:,i)==1));
-%     ind(X_train(:,i)==1) = i;
-%     testind(X_test(:,i)==1) = i;
-% end
-% 
-% Ynew = bsxfun(@minus, Y_train, meancity(ind));
-% fit = cvglmnet(X_train(:, 8:end), Ynew);
-% prices = cvglmnetPredict(fit, X_test(:, 8:end));
-% 
-% prices = bsxfun(@plus, prices, meancity(testind));
 
-%%
 X{7}=[];
 Y{7}=[];
 fit{7}=[];
