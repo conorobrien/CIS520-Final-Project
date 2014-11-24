eab0ff82edea2e4a8ffe5deaaf22f521e53e66c3
Conor O'Brien <conoreobrien@gmail.com>
2014-11-24 18:23:48 -0500

    fiddled with predict_bins and run_submission

--- code/run_submission.m
+++ code/run_submission.m
@@ -22,7 +22,7 @@ X_pca = spca([X_train; X_test], 150);
 X_pca_train = [city_train X_pca(1:length(X_train),:)];
 X_pca_test = [city_test X_pca((length(X_train)+1):end,:)];
 
-[Yhat_bins, Y_bins] = predict_bins(X_pca_train, Y_train, X_pca_test, 10);
+[Yhat_bins, Y_bins] = predict_bins(X_pca_train, Y_train, X_pca_test, 5);
 
 
 %% Save results to a text file for submission
