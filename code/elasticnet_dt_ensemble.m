36853f4ee444884f30aa3eb7a2bd25b54a93a1ab
Nick Iodice <nmiodice@gmail.com>
2014-12-01 18:05:11 -0500

    predicted on test set using mix of elastic net and decesion trees (on residual)

--- code/elasticnet_dt_ensemble.m
+++ code/elasticnet_dt_ensemble.m
@@ -1,3 +1,7 @@
+%% control variables
+full_prediction = 1;
+cross_val = 0;
+
 %% CVGLMNET for different cities, then use a DT forest to fit the residual
 load ../data/city_train.mat
 load ../data/city_test.mat
@@ -7,12 +11,27 @@ load ../data/bigram_train.mat
 load ../data/bigram_test.mat
 load ../data/price_train.mat
 
-
 x_train = [city_train word_train bigram_train];
 y_train = price_train;
 x_test = [city_test word_test bigram_test];
 
+% if a pool is open, close it
+try
+    matlabpool('close');
+catch err
+end
+
 worker_pool = parpool();
-mse = crossval('mse', x_train, y_train,'Predfun', ...
-    @elasticnet_dt_ensemble_pred_fun, 'kfold', 2)
+
+if cross_val == 1
+    mse = crossval('mse', x_train, y_train,'Predfun', ...
+        @elasticnet_dt_ensemble_pred_fun, 'kfold', 2)
+end
+
+if full_prediction == 1
+    yfit = elasticnet_dt_ensemble_pred_fun(x_train, y_train, x_test);
+    dlmwrite('submit.txt', yfit)
+end
+
+
 delete(worker_pool);
