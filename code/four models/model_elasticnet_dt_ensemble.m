ca11d0899ec88154f8d8d2aeae238488be27d28d
Nick Hu <siyaohu@seas.upenn.edu>
2014-12-04 15:53:30 -0500

    added draft final submissions

---
+++ code/four models/model_elasticnet_dt_ensemble.m
@@ -0,0 +1,39 @@
+function [yfit] = model_elasticnet_dt_ensemble(x_train, y_train, x_test)
+    addpath('glmnet_matlab')
+    X{7} = [];
+    Y{7} = [];
+    residuals{7} = [];
+    cvglmnet_fit{7} = [];
+    tree_fit{7} = [];
+    options = glmnetSet();
+    options.alpha = 0;
+    tree_stats = statset('UseParallel', true);
+    % Split data out by cities. Then, train each city individually
+    for i = 1:7
+        city_idxs = x_train(:, i) == 1;
+        X{i} = x_train(city_idxs, 8:end);
+        Y{i} = y_train(city_idxs);
+        cvglmnet_fit{i} = cvglmnet(X{i}, Y{i}, 'gaussian', options);
+
+        y_hat = cvglmnetPredict(cvglmnet_fit{i}, X{i});
+
+        % train on the residuals
+        residuals{i} = y_hat - Y{i};
+        tree_fit{i} = TreeBagger(1, full(X{i}), residuals{i}, 'method', 'regression', 'Options', tree_stats,'MinLeaf', 2);
+        tree_fit{i} = compact(tree_fit{i});
+
+        disp(['trained city # ', num2str(i)]);
+    end
+
+    % generate prediction based on the elastic net and residual computations
+    yfit = zeros(size(x_test, 1), 1);
+    for i = 1:7
+        city_idxs = x_test(:, i) == 1;
+        X{i} = x_test(city_idxs, 8:end);
+        base_fit = cvglmnetPredict(cvglmnet_fit{i}, X{i});
+        residual_fit = predict( tree_fit{i}, full(X{i}));
+        yfit(city_idxs) = base_fit - residual_fit;
+    end
+
+
+end
--- code/four models/model_pca.m
+++ code/four models/model_pca.m
@@ -1,25 +1,24 @@
-function yfit = model_elasticnet(x_train, y_train, x_test)
+function yfit = model_pca(x_train,y_train, x_test)
+
+    [~,~,V] = svds([x_train(:,8:end); x_test(:, 8:end)], 50);
 
     addpath('glmnet_matlab')
 
-    options = glmnetSet();
-    options.alpha = 0.2;
-    
     X{7}=[];
     Y{7}=[];
     fit{7}=[];
     for i = 1:7
         city_idxs = x_train(:, i) == 1;
-        X{i} = x_train(city_idxs, 8:end);
+        X{i} = x_train(city_idxs, 8:end)*V;
         Y{i} = y_train(city_idxs);
-        fit{i} = cvglmnet(X{i}, Y{i}, 'gaussian', options);
+        fit{i} = cvglmnet(X{i}, Y{i});
         disp(['trained city # ', num2str(i)]);
     end
     yfit = zeros(size(x_test, 1), 1);
-    %%
+
     for i = 1:7
         city_idxs = x_test(:, i) == 1;
-        X{i} = x_test(city_idxs, 8:end);
+        X{i} = x_test(city_idxs, 8:end)*V;
         yfit(city_idxs)  = cvglmnetPredict( fit{i}, X{i});
     end
     
